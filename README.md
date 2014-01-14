80b26e749980c846f7a62d10e5c9f9c61d0fc6f5
Erik van het Hof <hof@hofcom.nl>
2014-01-14 21:03:35 +0100

    Add compiled jar with dependencies.

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 basis-java-api
 ==============
 
-Library to access your Basis device data
+Library to download your raw Basis B1 device data from the mybasis website. 
+You can learn more about Basis at [http://www.mybasis.com/](http://www.mybasis.com/)
+
+
--- src/main/java/org/hofcom/basis/BasisDownload.java
+++ src/main/java/org/hofcom/basis/BasisDownload.java
@@ -8,8 +8,6 @@ import java.util.concurrent.ExecutionException;
  * @author hof
  */
 public class BasisDownload {
-
-    private final static BasisApi basisApi = new BasisApi(); 
     
     /**
      * @param args the command line arguments
@@ -18,20 +16,20 @@ public class BasisDownload {
             throws IOException, ExecutionException, InterruptedException {
         
         System.out.println("Basis Data Download 0.1.");
-        System.out.println("Erik van het Hof hof@hofcom.nl Twitter: @erikvanhethof");
+        System.out.println("Erik van het Hof (hof@hofcom.nl) Twitter: @erikvanhethof");
         
         if (args.length != 5) { 
             System.out.println(("\nUsage: BasisDownload <username> <password> <day> <start_offset> <end_offset>"));
-            System.out.println("Example: BasisDownload hof@hofcom.nl password 2014-01-14 0 0"); 
+            System.out.println("\nExample: BasisDownload hof@hofcom.nl password 2014-01-14 0 0"); 
             return; 
         }
-        
+
+        BasisApi basisApi = new BasisApi(); 
         basisApi.authenticate(args[0], args[1]);
         basisApi.getMe(); 
         basisApi.getData(args[2], args[3], args[4]);
         basisApi.getActivities(args[2]); 
         basisApi.getHttpClient().close(); 
         
-    }
-    
+    }    
 }
